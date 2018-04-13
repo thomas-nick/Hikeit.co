3a1fbf57e1b5ad100598b5c2fc93b73970a09e03
Nicholas Thomas <nthomas1999@gmail.com>
2018-04-13 06:48:12 -0600

    hikeit DAPP

---
+++ contracts/DappToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.2;
+
+/**
+ * The Dapp Token contract does this and that...
+ */
+contract DappToken {
+    // Consructor
+    // Number of Tokens
+    // Read Total of tokens
+    uint256 public totalSupply;
+    function DappToken () public {
+        totalSupply = 1000000; // state variable
+
+    }
+}
